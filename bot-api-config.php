6decb8ca9203a0f9cfbabbc0cb6ae690baed10da
milkhamil <32932970+milkhamil@users.noreply.github.com>
2023-05-10 22:18:15 +0700

    Update bot-api-config.php

--- bot-api-config.php
+++ bot-api-config.php
@@ -4,7 +4,7 @@ if (!defined('HS')) {
     die('Tidak boleh diakses langsung.');
 }
 
-$token = '';
+$token = '6291664857:AAEstgKlJclMivUFxypcLtKIo5NFp0MOWPg';
 $debug = true;
 
 $chatid = '213567634';
